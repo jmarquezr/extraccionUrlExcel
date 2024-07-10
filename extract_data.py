77abb348b6c8376c1d7c234a7dd8ff3bb887d620
Jorge Marquez <jmarqzr@gmail.com>
2024-07-10 16:35:32 -0500

    se modifico para que acepte una o varias urls desde un archivo el extract_data.py

--- extract_data.py
+++ extract_data.py
@@ -3,24 +3,43 @@ import pandas as pd
 import os
 from bs4 import BeautifulSoup
 
-DATA_URL = 'https://es.wikipedia.org/wiki/Anexo:Destinos_tur%C3%ADsticos_mundiales'
-OUTPUT_FILE = 'D:\\paractica1\\data.xlsx'
+#DATA_URL = 'https://es.wikipedia.org/wiki/Anexo:Destinos_tur%C3%ADsticos_mundiales'
+
+
+# Abrir el archivo de configuración
+with open('urls.txt', 'r') as file:
+    # Leer todas las líneas
+    urls = file.readlines()
+    
+    # Quitar los caracteres de nueva línea al final de cada línea
+    urls = [url.strip() for url in urls]
+
+
+#OUTPUT_FILE = 'D:\\paractica1\\data.xlsx'
+OUTPUT_FILE = 'D:\\paractica1\\data'
+
 
 def extract_data():
-    response = requests.get(DATA_URL)
-    soup = BeautifulSoup(response.content, 'html.parser')
 
-    # Extract table data
-    table = soup.find_all('table')[0]  # Assuming the first table contains the relevant data
-    df = pd.read_html(str(table))[0]
+    contador=1
+    for DATA_URL in urls:
+        response = requests.get(DATA_URL)
+        soup = BeautifulSoup(response.content, 'html.parser')
 
-    # Create output directory if it doesn't exist
-    output_dir = os.path.dirname(OUTPUT_FILE)
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
+        # Extract table data
+        table = soup.find_all('table')[0]  # Assuming the first table contains the relevant data
+        df = pd.read_html(str(table))[0]
+        
+        new_path = os.path.join(OUTPUT_FILE, contador,'.xlsx')
 
-    df.to_excel(OUTPUT_FILE, index=False)
-    print(f'Data extracted and saved to {OUTPUT_FILE}')
+        # Create output directory if it doesn't exist
+        output_dir = os.path.dirname(new_path)
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+        df.to_excel(OUTPUT_FILE, index=False)
+        print(f'Data extracted and saved to {OUTPUT_FILE}')
+
 
 if __name__ == "__main__":
     extract_data()
